24e8ccded75a8bba6c41fabc9afc8883ac683965
Zoan112 <zohar.hadari@gmail.com>
2021-07-11 16:29:38 +0300

    Added latest data functionality

--- Routes/get.js
+++ Routes/get.js
@@ -19,5 +19,19 @@ router.get('/livingrooms', cors() ,async (req, res)=>{
 
     
     })
+
+/*Return latest entry*/
+    router.get('/livingrooms%20-l', cors() , async (req, res)=>{
+        try{ 
+           const results = await Post.find().limit(1).sort({$natural:-1}) ;
+            res.json(results);
+        }
+        catch(err){
+            res.json({meesage: err}); 
+        }
+    
+    
+        
+        })
     
 module.exports = router;
